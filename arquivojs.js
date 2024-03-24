d5ae62fb12180249cc12ebcdc6301d172feda7ab
Lucas <oliveira00904@gmail.com>
2024-03-24 18:33:33 -0300

    Primeiro commit dos desafios Dio

---
+++ arquivojs.js
@@ -0,0 +1,29 @@
+let nome = "Lucas"  
+let xp = []
+let patente = []
+
+if(xp<=1000){
+    patente= "Ferro"
+    console.log("O Herói de nome " + nome + " está na patente " + patente)
+}else if(xp>=1001 && xp<=2000){
+    patente= "Bronze"
+    console.log("O Herói de nome " + nome + " está na patente " + patente)
+}else if(xp>=2001 && xp<=5000){
+    patente= "Prata"
+    console.log("O Herói de nome " + nome + " está na patente " + patente)
+}else if(xp>=5001 && xp<=7000){
+    patente= "Ouro"
+    console.log("O Herói de nome " + nome + " está na patente " + patente)
+}else if(xp>=7001 && xp<=8000){
+    patente= "Platina"
+    console.log("O Herói de nome " + nome + " está na patente " + patente)
+}else if(xp>=8001 && xp<=9000){
+    patente= "Ascendente"
+    console.log("O Herói de nome " + nome + " está na patente " + patente)
+}else if(xp>=9001 && xp<=10000){
+    patente= "Imortal"
+    console.log("O Herói de nome " + nome + " está na patente " + patente)
+}else{
+    patente="Radiante"
+    console.log("O Herói de nome " + nome + " está na patente " + patente)
+}
---
+++ códigos-desafios-dio2.js
@@ -0,0 +1,24 @@
+let vitorias = document.getElementById("vitorias")
+let derrotas 
+let resultado = vitorias - derrotas
+let nivel
+function saldoVitorias(resultado) {
+    if(resultado<10){
+        nivel = "Ferro"
+    }else if(resultado>11 && resultado<20){
+        nivel = "Bronze"
+    }else if(resultado>21 && resultado<50){
+        nivel = "Prata"
+    }else if(resultado>51 && resultado<80){
+        nivel = "Ouro"
+    }else if(resultado>81 && resultado<90){
+        nivel = "Diamante"
+    }else if(resultado>91 && resultado<100){
+        nivel = "Lendário"
+    }else if(resultado>=101){
+        nivel = "Imortal"
+    }
+    return resultado
+}
+
+console.log("O Herói tem de saldo de " + saldoVitorias(resultado) + " está no nível de " + nivel)
